cb6f10c57916166c7c4958ef22cada56a9ba0671
ChristianCoba <danielcoba2820@gmail.com>
2025-11-14 19:11:54 -0500

    Inicio para netli7

---
+++ frontend/vite.config.js
@@ -0,0 +1,21 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, 'pages/404.html'),
+        about: resolve(__dirname, 'pages/about.html'),
+        admin: resolve(__dirname, 'pages/admin.html'),
+        car: resolve(__dirname, 'pages/car.html'),
+        destinations: resolve(__dirname, 'pages/destinations.html'),
+        login: resolve(__dirname, 'pages/login.html'),
+        register: resolve(__dirname, 'pages/register.html'),
+        tours: resolve(__dirname, 'pages/tours.html'),
+        user: resolve(__dirname, 'pages/user.html'),
+      }
+    }
+  }
+})
