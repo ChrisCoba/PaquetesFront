2d44c0e0a125183fc97a9830607f649d96ddd63f
ChristianCoba <danielcoba2820@gmail.com>
2025-12-01 08:46:42 -0500

    Restored clean SoapClient.js

--- src/services/soap/SoapClient.js
+++ src/services/soap/SoapClient.js
@@ -50,6 +50,63 @@ export const SoapClient = {
       ${paramXml}
     </${action}>
   </soap:Body>
+</soap:Envelope>`;
+    },
+
+    toXml(obj, key = null) {
+        let xml = '';
+
+        if (Array.isArray(obj)) {
+            // For arrays, we usually repeat the tag for each item, 
+            // BUT in SOAP/ASMX, it depends on the WSDL. 
+            // Often it's <turistas><TuristaSoap>...</TuristaSoap><TuristaSoap>...</TuristaSoap></turistas>
+            // OR just repeating the element if it's "maxOccurs=unbounded"
+            // For this specific ASMX, 'turistas' is likely the array wrapper, and inside we need the item type.
+            // However, standard ASMX arrays usually look like:
+            // <turistas>
+            //   <TuristaSoap>...</TuristaSoap>
+            //   <TuristaSoap>...</TuristaSoap>
+            // </turistas>
+            // So if the key is 'turistas', we iterate.
+
+            for (const item of obj) {
+                // We need to know the item tag name. 
+                // For 'turistas', it's 'TuristaSoap'. 
+                // This is a bit hard to genericize without WSDL.
+                // We'll try a heuristic: use the singular or a specific mapping if needed.
+                // For now, let's assume the object inside has the correct structure or we pass the wrapper.
+
+                // Actually, the caller should pass the structure:
+                // params = { turistas: [ { TuristaSoap: { ... } }, ... ] }
+                // OR we handle specific keys here.
+
+                // Let's try to just process the item. If the item is an object, it should generate its own tags.
+                // But we need the wrapping tag for the item.
+
+                // Heuristic: if key is 'turistas', item tag is 'TuristaSoap'.
+                let itemKey = 'item';
+                if (key === 'turistas') itemKey = 'TuristaSoap';
+
+                xml += this.toXml(item, itemKey);
+            }
+        } else if (typeof obj === 'object' && obj !== null) {
+            // If it has a key, wrap it.
+            if (key) xml += `<${key}>`;
+
+            for (const [prop, value] of Object.entries(obj)) {
+                xml += this.toXml(value, prop);
+            }
+
+            if (key) xml += `</${key}>`;
+        } else {
+            // Primitive value
+            if (key) xml += `<${key}>${obj}</${key}>`;
+        }
+
+        return xml;
+    },
+
+    /**
      * Parses the XML response to a JS Object
      * Note: This is a simple parser for the specific structure we expect.
      * For complex objects, we might need a more robust XML parser.
@@ -66,7 +123,7 @@ export const SoapClient = {
         }
 
         // Get Result
-        const resultTag = `${ action } Result`;
+        const resultTag = `${action}Result`;
         const resultNode = xmlDoc.getElementsByTagName(resultTag)[0];
 
         if (!resultNode) return null;
@@ -75,6 +132,22 @@ export const SoapClient = {
         if (resultNode.children.length > 0) {
             return this.xmlToJson(resultNode);
         }
+
+        // Simple value
+        return resultNode.textContent;
+    },
+
+    xmlToJson(xml) {
+        // Create the return object
+        var obj = {};
+
+        if (xml.nodeType == 1) { // element
+            // do attributes
+            if (xml.attributes.length > 0) {
+                obj["@attributes"] = {};
+                for (var j = 0; j < xml.attributes.length; j++) {
+                    var attribute = xml.attributes.item(j);
+                    obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
                 }
             }
         } else if (xml.nodeType == 3) { // text
