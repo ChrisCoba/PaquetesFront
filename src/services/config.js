a60ce7fa68f0fbc539912f960de666817fa74bd6
ChristianCoba <danielcoba2820@gmail.com>
2025-11-30 20:24:53 -0500

    Cambios para SOAP, el switch no estaba funcionando se publico en otro Hosting el SOAP

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
 
 export const API_BASE_URL = "https://worldagency.runasp.net/api/v1/integracion/paquetes";
-export const SOAP_API_URL = 'https://worldagency.runasp.net/WS_GestionPaquetes/PaquetesService.asmx';
+export const SOAP_API_URL = 'https://worldagencysoa.runasp.net/PaquetesService.asmx';
 export const USE_SOAP = { value: false }; // Object to allow reference updates
